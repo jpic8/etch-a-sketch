7b973d64400b8e8c954e7f29d0b4930ab2020976
jpic8 <joshua.pickett@gmail.com>
2021-12-29 15:25:44 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//script.js
+
+//DOM elements
+const container = document.querySelector("#container");
+const reset = document.querySelector("#reset");
+
+//function square creates a single div and changes the color on mouseover
+function square() {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("newSquare");
+  newDiv.addEventListener("mouseover", function (event) {
+    event.target.style.backgroundColor = "black";
+  });
+  container.appendChild(newDiv);
+}
+
+//function grid fills the container with squares based off predetermined CSS sizing
+function grid() {
+  for (i = 0; i < 6400; i++) {
+    square();
+  }
+}
+
+grid();
